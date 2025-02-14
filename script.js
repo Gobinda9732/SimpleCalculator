4fdd0c860232cac8e13086306e953e37e79442d8
Gobinda9732 <gobindajana9732@gmail.com>
2025-02-14 20:04:42 +0530

    add SimpleCalculator local to remote

---
+++ script.js
@@ -0,0 +1,5 @@
+let num2 = document.querySelector("#num2").value;
+let num1 =document.querySelector("#num1").value;
+let operation = document.querySelector("#option").value;  
+let Res_container = document.querySelector("#result").innerText;  
+   
