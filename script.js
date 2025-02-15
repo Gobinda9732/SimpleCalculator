37fec50e345d81289f1cdf1e5d4b80eb3825ac7f
Gobinda9732 <gobindajana9732@gmail.com>
2025-02-15 18:47:06 +0530

    fix num2 error message

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
-let num2 = document.querySelector("#num2").value;
-let num1 =document.querySelector("#num1").value;
-let operation = document.querySelector("#option").value;  
-let Res_container = document.querySelector("#result").innerText;  
+function calculate(){
+    let num1 =parseFloat(document.querySelector("#num1").value);
+    let num2 = parseFloat(document.querySelector("#num2").value);
+    let operation = document.querySelector("#option").value;  
+    let res=0;
+    // let Res_container = document.querySelector("#result").innerText; 
+    
+    function Error(){
+        let para=document.createElement("p");
+        para.innerText="number2 can't zero";
+        para.style.color="red";
+    
+        let num2err=document.querySelector(".secondNumber");
+        num2err.append(para);
+        res="";
+    
+    }
+    
+    
+    
+    switch(operation){
+        case "+":
+            res=num1+num2;
+            break;
+        case "-":
+            res=num1-num2;
+            break;
+        case "*":
+            res=num1*num2;
+            break;
+        case "/":
+            (num2!==0)? res=parseFloat(num1/num2).toFixed(2) :Error();
+            break;
+        case "%":
+            (num2!==0)? res=parseFloat(num1%num2).toFixed(2) :Error();
+            break;
+        default:
+            res="choose the currect option";
+    }
+    document.querySelector("#result").innerText =res;
+
+}
+
+
+//document.location.reload();
+ 
    
